d6da5b444a662eab615a3f84b0b94e5170ab4f6e
nimeullaha71 <nimeullahacse@gmail.com>
2025-07-11 14:54:57 +0600

    login screen create

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:taskmanagement_live/ui/screens/login_screen.dart';
+import 'package:taskmanagement_live/ui/utils/assets_path.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,10 +11,36 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  Future<void>_moveToNextScreen()async{
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SvgPicture.asset('assets/images/background.svg'),
+
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            AssetsPath.backgroundSvg,
+            fit: BoxFit.cover,
+
+          ),
+          Center(child: SvgPicture.asset(AssetsPath.logoSvg,width: 120,),),
+
+        ],
+      ),
     );
   }
 }
